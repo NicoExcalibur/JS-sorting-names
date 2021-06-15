a1d7bf891d15a41d454dafb76e0b0a95022d7afa
NicoExcalibur <nico.zanz@gmail.com>
2021-06-15 11:07:55 +0200

    display the list

--- app.js
+++ app.js
@@ -18,7 +18,10 @@ function strip(bandName) {
   return bandName.replace(/^(a |the |an )/i, "").trim(); // regex
 }
 
-const sortedBands = bands.sort((a, b) => 
-  return strip(a) > strip(b) ? 1 : -1;
-);
+const sortedBands = bands.sort((a, b) => (strip(a) > strip(b) ? 1 : -1));
+
+document.querySelector("#bands").innerHTML = sortedBands
+  .map((band) => `<li>${band}</li>`)
+  .join(""); // take the "," out of the string
+
 console.log(sortedBands);
